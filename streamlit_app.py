4c1326401435eda660d845ffafac186f6de12e3b
juluri sreekar <121714767+herohero0203@users.noreply.github.com>
2022-12-31 15:43:58 +0530

    Update streamlit_app.py
    added dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,12 @@ streamlit.text('🥑🍞 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
 streamlit.header('build your own fruit smoothie')
+
+
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
+
+streamlit.dataframe(my_fruit_list)
